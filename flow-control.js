6d58854dfed14193d8612979ae9743a6cf658fb5
Shelby Ro <shelby_ro@yahoo.com>
2020-05-12 18:19:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -43,3 +43,10 @@ function ageChecker(age){
     return "You are a grownup";
   }
 }
+function ternaryTeenager(age){
+  if(age>=13 && age<=19){
+    return "You are a teenager";
+  }else if (age<13||age>19){
+    return "You are not a teenager";
+  }
+}
