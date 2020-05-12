16e88735ad79faab7b61c64df8a2dec5241f2d39
Shelby Ro <shelby_ro@yahoo.com>
2020-05-12 18:28:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -50,3 +50,29 @@ function ternaryTeenager(age){
     return "You are not a teenager";
   }
 }
+function switchAge(age){
+  switch(age){
+    case 13:
+    return "You are a teenager";
+    break;
+    case 14:
+    return "You are a teenager";
+    break;
+    case 15:
+    return "You are a teenager";
+    break;
+    case 16:
+    return "You are a teenager";
+    break;
+    case 17:
+    return "You are a teenager";
+    break;
+    case 18:
+    return "You are a teenager";
+    break;
+    case 19:
+    return "You are a teenager";
+    default:
+    return "You have an age"
+  }
+}
